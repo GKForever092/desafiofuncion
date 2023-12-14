b24f3452214db939d2b3b3cd2f79d2ec3fc47036
Felipe Aguilar <aguilarfelipe092@gmail.com>
2023-12-13 22:10:26 -0300

    Cambio desafío 2

--- desa2/assets/js/script2.js
+++ desa2/assets/js/script2.js
@@ -1,10 +1,10 @@
-var select = document.querySelector("#sselect")
-var sticmax = document.querySelector("#maxs")
+let select = document.querySelector("#sselect")
+let sticmax = document.querySelector("#maxs")
 
 function total(){
-    var input1 = parseFloat(document.getElementById("s1").value)
-    var input2 = parseFloat(document.getElementById("s2").value)
-    var input3 = parseFloat(document.getElementById("s3").value)
+    let input1 = parseFloat(document.getElementById("s1").value)
+    let input2 = parseFloat(document.getElementById("s2").value)
+    let input3 = parseFloat(document.getElementById("s3").value)
     
     selectTotal = parseInt(sticmax.innerText)
     sticmax.innerText = (input1 + input2 + input3)
--- desa3/assets/js/script3.js
+++ desa3/assets/js/script3.js
@@ -1,15 +1,15 @@
-var select1 = document.getElementById("sel1")
-var select2 = document.getElementById("sel2")
-var select3 = document.getElementById("sel3")
-var result = document.querySelector("#text")
+let select1 = document.getElementById("sel1")
+let select2 = document.getElementById("sel2")
+let select3 = document.getElementById("sel3")
+let result = document.querySelector("#text")
 
 function aaa() {
 
-    var valor1 = select1.value
-    var valor2 = select2.value
-    var valor3 = select3.value
+    let valor1 = select1.value
+    let valor2 = select2.value
+    let valor3 = select3.value
 
-    var combinedResult = valor1 + valor2 + valor3;
+    let combinedResult = valor1 + valor2 + valor3;
     result.textContent = combinedResult
 
     if (valor1 === "9" && valor2 === "1" && valor3 === "1" || valor1 === "7" && valor2 === "1" && valor3 === "4"){
